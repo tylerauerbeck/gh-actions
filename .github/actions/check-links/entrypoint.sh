1479f883464149e9734ccae019dca51a7b3a24aa
Tyler Auerbeck <tauerbec@redhat.com>
2019-10-06 19:40:32 -0400

    More testing

--- .github/actions/check-links/entrypoint.sh
+++ .github/actions/check-links/entrypoint.sh
@@ -4,4 +4,4 @@
 echo "Checking the following files:"
 cat /github/home/pr-files
 
-for i in $(cat /github/home/pr-files);do markdown-link-check $GITHUB_WORKSPACE/$i;done
+for i in $(cat /github/home/pr-files);do echo $GITHUB_WORKSPACE/$i; ls -ld $GITHUB_WORKSPACE/$i; markdown-link-check $GITHUB_WORKSPACE/$i;done
