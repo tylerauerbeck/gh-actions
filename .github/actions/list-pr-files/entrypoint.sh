a84f348147ee95126a523aa4723581a8f3479b8b
Tyler Auerbeck <tauerbec@redhat.com>
2019-10-05 20:54:48 -0400

    Update entry

--- .github/actions/list-pr-files/entrypoint.sh
+++ .github/actions/list-pr-files/entrypoint.sh
@@ -2,6 +2,6 @@
 
 cat $GITHUB_EVENT_PATH
 
-PR_NUMBER=$(jq -r ".pull_request.self.href" "$GITHUB_EVENT_PATH")
+PR_NUMBER=$(jq -r ".pull_request._links.self.href" "$GITHUB_EVENT_PATH")
 
 echo "Collecting information about PR #$PR_NUMBER of $GITHUB_REPOSITORY..."
