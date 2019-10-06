71fd4422f8e0dd8a02ed96397bba5c589d1cf643
Tyler Auerbeck <tauerbec@redhat.com>
2019-10-06 19:45:54 -0400

    More testing

--- .github/actions/list-pr-files/entrypoint.sh
+++ .github/actions/list-pr-files/entrypoint.sh
@@ -4,6 +4,6 @@ PR_ENDPOINT=$(jq -r ".pull_request._links.self.href" "$GITHUB_EVENT_PATH")
 
 echo "Collecting changed files from $GITHUB_REPOSITORY"
 
-curl -s https://api.github.com/repos/tylerauerbeck/gh-actions/pulls/2/files | jq .[].filename > /github/home/pr-files
+curl -s https://api.github.com/repos/tylerauerbeck/gh-actions/pulls/2/files | jq -r .[].filename > /github/home/pr-files
 
 
