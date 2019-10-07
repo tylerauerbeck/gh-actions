79424ca106d7ce67a67be543d6faf05846c93ffd
Tyler Auerbeck <tauerbec@redhat.com>
2019-10-07 15:09:13 -0400

    More testing

---
+++ .github/actions/publish-to-dev.to/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get changed markdown file
+
+# Get Root of file (directory path)
+
+# Get metadata file (?)
+
+# If there's no metadata file, let's make some assumptions
+
+	# Build title from filename
+	# Check if there are labels
+		# if there are labels let's build the labels to apply
+		# if there are labels let's build the series name
+
+# if the action is a pull request, publish: false
+
+# if the action is a merge, publish : true
+
+cat /github/home/pr-files | egrep -i *.md$
