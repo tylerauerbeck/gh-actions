37f26a6a1bc4fcc2fd7ea25203a0b2ab70f7c72f
Tyler Auerbeck <tauerbec@redhat.com>
2019-10-06 20:31:54 -0400

    More

--- .github/actions/lint-markdown/entrypoint.sh
+++ .github/actions/lint-markdown/entrypoint.sh
@@ -4,4 +4,4 @@
 echo "Checking the following files:"
 cat /github/home/pr-files | egrep -i *.md
 
-for i in $(cat /github/home/pr-files | grep -i *.md);do remark $GITHUB_WORKSPACE/$i --use preset-lint-markdown-style-guide;done
+for i in $(cat /github/home/pr-files | egrep -i *.md);do remark $GITHUB_WORKSPACE/$i --use preset-lint-markdown-style-guide;done
