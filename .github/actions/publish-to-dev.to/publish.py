e87704a498a5a66503251c30cf992f638b6608c0
Tyler Auerbeck <tauerbec@redhat.com>
2019-10-10 10:28:18 -0400

    Add some logic

--- .github/actions/publish-to-dev.to/publish.py
+++ .github/actions/publish-to-dev.to/publish.py
@@ -7,6 +7,7 @@ API_ENDPOINT = "https://dev.to/api/articles"
 
 PR = os.getenv('PR_LIST_FILE')
 API_KEY = os.getenv('DEV_TO_TOKEN')
+STATUS=0
 
 with open(PR, 'r') as files:
     for line in files.readlines():
@@ -14,7 +15,12 @@ with open(PR, 'r') as files:
             with open(line.strip(),'r') as file:
                 data = file.read().replace('\n', '\\n')
                 json = '{"article":{"body_markdown": "' + data + '"}}'
-                print(json)
             headers = {'content-type': 'application/json; charset=utf-8', 'api-key': API_KEY}
             r = requests.post(url = API_ENDPOINT, data = json.encode('utf-8'), headers = headers)
-            print(r)
+            if r.Response != 201:
+                print("Unable to publish: " + line.strip())
+                STATUS=1
+
+if STATUS != 0:
+    print("There were issues publishing one or more files. Please see logs above for more details")
+            
