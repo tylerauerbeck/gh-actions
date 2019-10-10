2ea4262b98f16657ad9eba21b6e452e9484b0af2
Tyler Auerbeck <tauerbec@redhat.com>
2019-10-10 09:50:25 -0400

    More testing

--- .github/actions/publish-to-dev.to/publish.py
+++ .github/actions/publish-to-dev.to/publish.py
@@ -1,16 +1,20 @@
 #import required libraries
 import requests
 import re
+import os
 
 API_ENDPOINT = "https://dev.to/api/articles"
 
-with open("$PR_LIST_FILE", 'r') as files:
+PR = os.getenv('PR_LIST_FILE')
+API_KEY = os.getenv('DEV_TO_TOKEN')
+
+with open(PR, 'r') as files:
     for line in files.readlines():
         if re.search("^.*.md$", line):
             with open(line.strip(),'r') as file:
                 data = file.read().replace('\n', '\\n')
                 json = '{"article":{"body_markdown": "' + data + '"}}'
                 print(json)
-            headers = {'content-type': 'application/json', 'api-key': $DEV_TO_TOKEN}
+            headers = {'content-type': 'application/json', 'api-key': API_KEY}
             #r = requests.post(url = API_ENDPOINT, data = json, headers = headers)
             #print(r)
